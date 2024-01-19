6ad8eaf4000a8ca4331a4d6e6f2dacbbeb67a830
Bazch <mikael.vidal@gmail.com>
2024-01-19 18:08:11 -0300

    Widget para mostrar resultado e valores de operações na tela da calculadora

---
+++ lib/widgets/ColunaDeResultados.dart
@@ -0,0 +1,26 @@
+import 'package:calculadora/widgets/Controladora.dart';
+import 'package:calculadora/widgets/Linha.dart';
+import 'package:calculadora/widgets/MostraExpressao.dart';
+import 'package:calculadora/widgets/MostraResultado.dart';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ColunaDeResultados extends StatelessWidget {
+  final controladora = Get.find<Controladora>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => Column(
+        children: [
+          MostraExpressao(expressao: '${controladora.primeiroNumero}'),
+          MostraExpressao(expressao: '${controladora.operacao}'),
+          MostraExpressao(expressao: '${controladora.segundoNumero}'),
+          Linha(),
+          MostraResultado(resultado: '${controladora.resultado}'),
+        ],
+      ),
+    );
+  }
+}
