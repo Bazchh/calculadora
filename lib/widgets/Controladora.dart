22788dfe29d627d66007875bded10637c1f7330d
Bazch <mikael.vidal@gmail.com>
2024-01-19 18:52:30 -0300

    Correção de erro na calculadora que não fazia operações

--- lib/widgets/Controladora.dart
+++ lib/widgets/Controladora.dart
@@ -1,98 +1,83 @@
- import 'package:get/get.dart';
+// ignore_for_file: curly_braces_in_flow_control_structures, prefer_interpolation_to_compose_strings, constant_pattern_never_matches_value_type
 
-class Controladora{
-var primeiroNumero  = '10'.obs;
-  var segundoNumero = '20'.obs;
-  var resultado     = '30'.obs;
-  var operacao      = '+'.obs;
+import 'package:get/get.dart';
 
+class Controladora {
+  var primeiroNumero = '1'.obs;
+  var segundoNumero = '2'.obs;
+  var resultado = '3'.obs;
+  var operacao = '+'.obs;
 
-  ac() {
-    primeiroNumero.value  = '0';
-    segundoNumero.value = '0';
-    resultado.value   = '0';
-    operacao.value    = '+';
+  ac(){
+  primeiroNumero.value = '0';
+  segundoNumero.value = '0';
+  resultado.value = '0';
+  operacao.value = '+';
   }
 
-  
+  adicionarNumero(String numero){
+    if(resultado.value == '0')
+    return resultado.value = numero;
+    if(resultado.value == '-0')
+    return resultado.value = '-' + numero;
 
-  adicionarNumero( String number ) {
-
-    if ( resultado.value == '0' ) 
-      return resultado.value = number;
-
-    if ( resultado.value == '-0' ){
-      return resultado.value = '-' + number;
-    }
-    
-    resultado.value = resultado.value + number;
-
+    resultado.value = resultado.value + numero;
   }
 
-  adicionaPontoDecimal() {
+  adicionaPontoDecimal(){
+    if(resultado.contains('.')) return;
 
-    if ( resultado.contains('.') ) return;
-
-    if ( resultado.startsWith('0') ){
+    if(resultado.startsWith('0')){
       resultado.value = '0.';
     } else {
       resultado.value = resultado.value + '.';
     }
-
   }
 
-  selecionarOperacao( String newOperation ) {
-
-    operacao.value   = newOperation;
+  selecionarOperacao(String novaOperacao){
+    operacao.value = novaOperacao;
     primeiroNumero.value = resultado.value;
-    resultado.value  = '0';
-
+    resultado.value = '0';
   }
 
- deletarUltimaEntrada() {
+  deletarUltimaEntrada (){
+    if(resultado.value.replaceAll('-', '').length > 1){
+      resultado.value = resultado.value.substring(0, resultado.value.length -1);
 
-    if ( resultado.value.replaceAll('-', '').length > 1 ) {
-      resultado.value = resultado.value.substring(0, resultado.value.length - 1 );
     } else {
       resultado.value = '0';
     }
-
   }
 
-  calcularResultado() {
-
-    double num1 = double.parse( primeiroNumero.value );
-    double num2 = double.parse( resultado.value );
-
+  calcularResultado(){
+    double num1 = double.parse(primeiroNumero.value);
+    double num2 = double.parse(resultado.value);
     segundoNumero.value = resultado.value;
 
-    switch( operacao.value ){
-
+    switch (operacao.value){
       case '+':
-        resultado.value = '${ num1 + num2 }';
+      resultado.value = '${num1 + num2}';
       break;
-
       case '-':
-        resultado.value = '${ num1 - num2 }';
+      resultado.value = '${num1 - num2}';
       break;
-
-      case '/':
-        resultado.value = '${ num1 / num2 }';
+      case '/': 
+      resultado.value = '${num1 / num2}';
       break;
-
       case 'X':
-        resultado.value = '${ num1 * num2 }';
+      resultado.value = '${num1 * num2}';
       break;
-
+      case '%':
+      resultado.value = '${num1 % num2}';
+      break;
       default:
-        return;
+      return;
     }
 
-    if ( resultado.value.endsWith('.0') ) {
-      resultado.value = resultado.value.substring(0, resultado.value.length - 2 );
+    if(resultado.value.endsWith('.0')){
+      resultado.value = resultado.value.substring(0,resultado.value.length - 2);
     }
 
   }
-
 
 }
